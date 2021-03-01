78859f1ddfa82001609c67a9bdba9709ccd3f1b5
takumi-hiraoka <takumi-hiraoka@takumi-hiraokanoMacBook-Air.local>
2021-03-01 14:18:04 +0900

    finish no.2

---
+++ sql02/prac.sql
@@ -0,0 +1,28 @@
+SELECT shohin_mei, hanbai_tanka,
+hanbai_tanka*2 AS "hanbai_tankax2"
+FROM Shohin;
+
+SELECT shohin_mei,hanbai_tanka, shohin_bunrui -- この順番に表示される
+FROM Shohin
+WHERE hanbai_tanka <> 500;
+
+SELECT shohin_mei, shohin_bunrui, torokubi
+FROM Shohin
+WHERE torokubi < '2009-09-27';
+
+SELECT shohin_mei, shiire_tanka
+FROM Shohin
+WHERE shiire_tanka IS NULL;
+
+SELECT shohin_mei, shiire_tanka
+FROM Shohin
+WHERE shiire_tanka IS NOT NULL;
+
+SELECT shohin_mei, shohin_bunrui, hanbai_tanka
+FROM Shohin
+WHERE NOT hanbai_tanka >= 1000;
+
+SELECT shohin_mei, shiire_tanka
+FROM Shohin
+WHERE shohin_bunrui = 'キッチン用品'
+AND hanbai_tanka >= 3000;
