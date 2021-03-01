b56c3dba088eb60a7a3b1fe15d1bcdec50d36253
takumi-hiraoka <takumi-hiraoka@takumi-hiraokanoMacBook-Air.local>
2021-03-01 14:26:46 +0900

    commentout by main

--- sql02/prac.sql
+++ sql02/prac.sql
@@ -22,7 +22,7 @@ SELECT shohin_mei, shohin_bunrui, hanbai_tanka
 FROM Shohin
 WHERE NOT hanbai_tanka >= 1000;
 
-SELECT shohin_mei, shiire_tanka
-FROM Shohin
-WHERE shohin_bunrui = 'キッチン用品'
-AND hanbai_tanka >= 3000;
+-- SELECT shohin_mei, shiire_tanka
+-- FROM Shohin
+-- WHERE shohin_bunrui = 'キッチン用品'
+-- AND hanbai_tanka >= 3000;
