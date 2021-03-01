238cd6f3ece8a24445440a6ee8cb0668a6b782c5
takumi-hiraoka <takumi-hiraoka@takumi-hiraokanoMacBook-Air.local>
2021-03-01 14:28:32 +0900

    plus one more by postgresql

--- sql02/prac.sql
+++ sql02/prac.sql
@@ -26,3 +26,8 @@ SELECT shohin_mei, shiire_tanka
 FROM Shohin
 WHERE shohin_bunrui = 'キッチン用品'
 AND hanbai_tanka >= 3000;
+
+SELECT shohin_mei, shiire_tanka
+FROM Shohin
+WHERE shohin_bunrui = 'キッチン用品'
+AND hanbai_tanka >= 3000;
